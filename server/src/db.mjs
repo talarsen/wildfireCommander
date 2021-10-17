8662d3aa0818d92612e7965e10e97d3195b21480
Tara Larsen <talarsen74@gmail.com>
2021-10-17 14:59:16 -0700

    Initial commit

---
+++ server/src/db.mjs
@@ -0,0 +1,35 @@
+import path from "path";
+import { URL } from "url";
+
+import dotenv from "dotenv";
+import pgp from "pg-promise";
+
+const db = initDb();
+
+export const getTasks = () => db.any("SELECT * FROM tasks");
+
+export const addTask = (name) =>
+  db.one("INSERT INTO tasks(name) VALUES($<name>) RETURNING *", { name });
+
+function initDb() {
+  let connection;
+
+  if (process.env.DATABASE_URL === undefined) {
+    dotenv.config({
+      path: path.join(new URL(".", import.meta.url).pathname, "../../.env"),
+    });
+    connection = {
+      user: "postgres",
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: 5442,
+    };
+  } else {
+    connection = {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+
+  return pgp()(connection);
+}
