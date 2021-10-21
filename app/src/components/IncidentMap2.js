4d18ed143273be20f3ecd73a1c609143369975c5
Tara Larsen <talarsen74@gmail.com>
2021-10-21 15:51:37 -0700

    initial commit setup incident2Map component

---
+++ app/src/components/IncidentMap2.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import GoogleMapReact from "google-map-react";
+
+const incidentMap2 = () => {
+  return (
+    <GoogleMapReact
+      bootstrapURLKEYS={{ key: "AIzaSyCNCt1VaBoUszbIOIat9x-jXMNn8V34ZeM" }}
+    ></GoogleMapReact>
+  );
+};
